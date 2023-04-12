562ae9761878ed76f85a9f1352db337987c22768
ironm <ironm@JJEMD>
2023-04-12 11:23:49 -0600

    Add project files.

---
+++ QR/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace QR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.imgQR = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(45, 292);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(139, 46);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar código";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Location = new System.Drawing.Point(185, 292);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(127, 46);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar Código";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(45, 264);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(267, 22);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // imgQR
+            // 
+            this.imgQR.Location = new System.Drawing.Point(45, 12);
+            this.imgQR.Name = "imgQR";
+            this.imgQR.Size = new System.Drawing.Size(267, 246);
+            this.imgQR.TabIndex = 3;
+            this.imgQR.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 359);
+            this.Controls.Add(this.imgQR);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.PictureBox imgQR;
+    }
+}
+
---
+++ QR/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace QR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtCodigo.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(txtCodigo.Text.Trim(), out qrCode);
+
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+            MemoryStream ms = new MemoryStream();
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imageTemporal = new Bitmap(ms);
+            var image = new Bitmap(imageTemporal, new Size(new Point(200, 200)));
+            imgQR.BackgroundImage = image;
+            image.Save("imagen.png", ImageFormat.Png);
+            btnGuardar.Enabled = true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Image image = (Image)imgQR.BackgroundImage.Clone();
+            SaveFileDialog CajaDialogoGuardar = new SaveFileDialog();
+            CajaDialogoGuardar.AddExtension = true;
+            CajaDialogoGuardar.Filter = "Image PNG (*.png)|*.png";
+            CajaDialogoGuardar.ShowDialog();
+            if (!string.IsNullOrEmpty(CajaDialogoGuardar.FileName))
+            {
+                image.Save(CajaDialogoGuardar.FileName, ImageFormat.Png);
+            }
+            image.Dispose();
+        }
+    }
+}
